90447c5713eef48beb406597feab932bedf10726
joaowoigt <joaowoigt@ciandt.com>
2022-06-01 15:05:31 -0300

    Implemented Detail Loading Screen

--- app/src/main/java/com/example/mvpstudy/presentation/detail/PokemonDetailFragment.kt
+++ app/src/main/java/com/example/mvpstudy/presentation/detail/PokemonDetailFragment.kt
@@ -1,7 +1,6 @@
 package com.example.mvpstudy.presentation.detail
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,13 +10,12 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
-import coil.load
 import com.example.mvpstudy.R
 import com.example.mvpstudy.databinding.FragmentDetailBinding
+import com.example.mvpstudy.databinding.LoadingScreenBinding
 import com.example.mvpstudy.presentation.detail.domain.model.DetailPokemon
 import com.example.mvpstudy.presentation.detail.domain.model.PokemonSpeciesEntry
 import com.example.mvpstudy.presentation.detail.tabs.DetailAdapter
-import com.example.mvpstudy.presentation.home.domain.model.PokedexEntry
 import com.example.mvpstudy.utils.FlowState
 import com.example.mvpstudy.utils.createPokemonCard
 import com.google.android.material.tabs.TabLayoutMediator
@@ -54,10 +52,10 @@ class PokemonDetailFragment : Fragment(), PokemonDetailContract.View {
         lifecycleScope.launchWhenStarted {
             presenter.detailPokemon.collect { flowState ->
                 when (flowState) {
-                    is FlowState.Loading -> showLoading()
+                    is FlowState.Loading -> showLoading(binding.pageLoadingScreen)
                     is FlowState.Success -> {
                         setupCard(flowState.data)
-                        hideLoading()
+                        hideLoading(binding.pageLoadingScreen)
                     }
                     is FlowState.Error -> {
                         showToastError(flowState.message)
@@ -72,9 +70,12 @@ class PokemonDetailFragment : Fragment(), PokemonDetailContract.View {
         lifecycleScope.launchWhenStarted {
             presenter.descriptionAndEvolution.collect { flowState ->
                 when (flowState) {
-                    is FlowState.Loading -> println("Loading")
-                    is FlowState.Success -> setupAdapter(flowState.data)
-
+                    is FlowState.Loading -> showLoading(binding.moreDetailLoading)
+                    is FlowState.Success -> {
+                        hideLoading(binding.moreDetailLoading)
+                        setupAdapter(flowState.data)
+                    }
+                    is FlowState.Error -> showToastError(flowState.message)
                 }
             }
         }
@@ -83,8 +84,11 @@ class PokemonDetailFragment : Fragment(), PokemonDetailContract.View {
 
     private fun setupCard(detailPokemon: DetailPokemon?) {
         with(binding) {
-            pokemonNumberTextView.text = detailPokemon?.id.toString()
-            card.createPokemonCard(detailPokemon?.sprites?.front_default, pokemonSpriteImageView)
+            detailPokemon?.let {
+                pokemonNameTextView.text = detailPokemon.name.replaceFirstChar { it.uppercase() }
+                pokemonNumberTextView.text = detailPokemon.id.toString()
+                card.createPokemonCard(detailPokemon.sprites.front_default, pokemonSpriteImageView)
+            }
         }
     }
 
@@ -93,28 +97,27 @@ class PokemonDetailFragment : Fragment(), PokemonDetailContract.View {
         binding.viewPager.adapter = adapter
         adapter.addNewItem(pokemonSpeciesEntry)
         TabLayoutMediator(binding.tabLayout, binding.viewPager, true) { tab, position ->
-            tab.text =  when(position) {
+            tab.text = when (position) {
                 0 -> "Description"
-                else -> {"fail"}
+                else -> "fail"
             }
         }.attach()
     }
 
-    private fun showLoading() {
-        binding.loadingScreen.root.visibility = View.VISIBLE
-        binding.loadingScreen.pokeballImageView.startAnimation(
-            AnimationUtils.loadAnimation(context, R.anim.rotate_indefinetly)
-        )
+    private fun showLoading(loadingView: LoadingScreenBinding) {
+        loadingView.apply {
+            root.visibility = View.VISIBLE
+            pokeballImageView.startAnimation(
+                AnimationUtils.loadAnimation(context, R.anim.rotate_indefinetly)
+            )
+        }
     }
 
-    private fun hideLoading() {
-        binding.loadingScreen.root.visibility = View.GONE
+    private fun hideLoading(loadingView: LoadingScreenBinding) {
+        loadingView.root.visibility = View.GONE
     }
-
 
     private fun showToastError(message: String) {
         Toast.makeText(context, message, Toast.LENGTH_LONG).show()
     }
-
-
 }

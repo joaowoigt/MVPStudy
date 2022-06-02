09458737a04c75e4d1901b8554e4b6ea333c3e26
joaowoigt <joaowoigt@ciandt.com>
2022-06-02 11:14:51 -0300

    Finalizing stats tab

--- app/src/main/java/com/example/mvpstudy/presentation/detail/tabs/DetailAdapter.kt
+++ app/src/main/java/com/example/mvpstudy/presentation/detail/tabs/DetailAdapter.kt
@@ -8,7 +8,7 @@ import com.example.mvpstudy.data.remote.model.Stat
 import com.example.mvpstudy.presentation.detail.PokemonDetailFragment
 import com.example.mvpstudy.presentation.detail.domain.model.PokemonSpeciesEntry
 
-class DetailAdapter(fragment: PokemonDetailFragment): FragmentStateAdapter(fragment) {
+class DetailAdapter(fragment: PokemonDetailFragment) : FragmentStateAdapter(fragment) {
 
     private var fragmentList = mutableListOf<Fragment>()
 
@@ -21,10 +21,24 @@ class DetailAdapter(fragment: PokemonDetailFragment): FragmentStateAdapter(fragm
     }
 
     fun addNewItem(stats: List<Stat>) {
-        fragmentList.add(StatsTabFragment.newInstance(stats[HPSTATINDEX].base_stat))
+        fragmentList.add(
+            StatsTabFragment.newInstance(
+                stats[HPSTATINDEX].base_stat,
+                stats[ATTACKSTATINDEX].base_stat,
+                stats[DEFENSESTATSINDEX].base_stat,
+                stats[SPATTACKSTATINDEX].base_stat,
+                stats[SPDEFENSESTATINDEX].base_stat,
+                stats[SPEEDSTATINDEX].base_stat,
+            )
+        )
     }
 
     companion object {
         const val HPSTATINDEX = 0
+        const val ATTACKSTATINDEX = 1
+        const val DEFENSESTATSINDEX = 2
+        const val SPATTACKSTATINDEX = 3
+        const val SPDEFENSESTATINDEX = 4
+        const val SPEEDSTATINDEX = 5
     }
 }
--- app/src/main/java/com/example/mvpstudy/presentation/detail/tabs/StatsTabFragment.kt
+++ app/src/main/java/com/example/mvpstudy/presentation/detail/tabs/StatsTabFragment.kt
@@ -2,16 +2,22 @@ package com.example.mvpstudy.presentation.detail.tabs
 
 import android.content.res.ColorStateList
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.mvpstudy.databinding.DetailTabStatsBinding
 
-class StatsTabFragment: Fragment() {
+class StatsTabFragment : Fragment() {
 
     private lateinit var binding: DetailTabStatsBinding
     private val hpValues by lazy { arguments?.getInt(HPVALUES) }
+    private val attackValues by lazy { arguments?.getInt(ATTACKVALUES) }
+    private val defenseValues by lazy { arguments?.getInt(DEFENSEVALUES) }
+    private val spAttackValues by lazy { arguments?.getInt(SPATTACKVALUES) }
+    private val spDefenseValues by lazy { arguments?.getInt(SPDEFENSEVALUES) }
+    private val speedValues by lazy { arguments?.getInt(SPEEDVALUES) }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,16 +30,38 @@ class StatsTabFragment: Fragment() {
     }
 
     private fun setupView() {
-        hpValues?.let {binding.hpStatsIndicator.progress = it }
+        hpValues?.let { binding.hpStatsIndicator.progress = it }
+        attackValues?.let { binding.attackStatsIndicator.progress = it }
+        defenseValues?.let { binding.defenseStatsIndicator.progress = it }
+        spAttackValues?.let { binding.spAttackStatsIndicator.progress = it }
+        spDefenseValues?.let { binding.spDefenseStatsIndicator.progress = it }
+        speedValues?.let { binding.speedStatsIndicator.progress = it }
     }
 
     companion object {
         private const val HPVALUES = "HPVALUES"
+        private const val ATTACKVALUES = "ATTACKVALUES"
+        private const val DEFENSEVALUES = "DEFENSEVALUES"
+        private const val SPATTACKVALUES = "SPATTACKVALUES"
+        private const val SPDEFENSEVALUES = "SPDEFENSEVALUES"
+        private const val SPEEDVALUES = "SPEEDVALUES"
         private const val COLOR = "COLOR"
 
-        fun newInstance(hpValues: Int) = StatsTabFragment().apply {
-            arguments?.apply {
+        fun newInstance(
+            hpValues: Int,
+            attackValues: Int,
+            defenseValues: Int,
+            spAttackValues: Int,
+            spDefenseValues: Int,
+            speedValues: Int,
+        ) = StatsTabFragment().apply {
+            arguments = Bundle().apply {
                 putInt(HPVALUES, hpValues)
+                putInt(ATTACKVALUES, attackValues)
+                putInt(DEFENSEVALUES, defenseValues)
+                putInt(SPATTACKVALUES, spAttackValues)
+                putInt(SPDEFENSEVALUES, spDefenseValues)
+                putInt(SPEEDVALUES, speedValues)
             }
         }
     }
